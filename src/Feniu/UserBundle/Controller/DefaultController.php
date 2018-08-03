e076004185d2edc9799367a1b28b13f2a0cbb8d4
root <root@merida.com.pl>
2018-08-03 15:01:15 +0200

    initial commit

---
+++ src/Feniu/UserBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Feniu\UserBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('FeniuUserBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Feniu/UserBundle/Entity/User.php
@@ -0,0 +1,71 @@
+<?php
+// src/AppBundle/Entity/User.php
+
+namespace Feniu\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
+    /**
+     * @ORM\Column(type="string", name="facebook_id", nullable=true)
+     */
+    protected $facebookId;
+
+    /**
+     * @ORM\Column(type="string", name="google_id", nullable=true)
+     */
+    protected $googleId;
+
+    /**
+     * @return mixed
+     */
+    public function getFacebookId()
+    {
+        return $this->facebookId;
+    }
+
+    /**
+     * @param mixed $facebookId
+     */
+    public function setFacebookId($facebookId)
+    {
+        $this->facebookId = $facebookId;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getGoogleId()
+    {
+        return $this->googleId;
+    }
+
+    /**
+     * @param mixed $googleId
+     */
+    public function setGoogleId($googleId)
+    {
+        $this->googleId = $googleId;
+    }
+}
